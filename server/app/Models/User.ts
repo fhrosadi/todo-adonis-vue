d8e8f9b77248b1edc024c4beb9b115124abaff55
reno <haikalreno13@gmail.com>
2022-01-27 13:49:09 +0700

    27 jan

--- server/app/Models/User.ts
+++ server/app/Models/User.ts
@@ -9,9 +9,6 @@ export default class User extends BaseModel {
   public id: number
 
   @column()
-  public username: string
-
-  @column()
   public email: string
 
   @column({ serializeAs: null })
